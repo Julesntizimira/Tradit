303a332c734a39ce3692783387366559f654955c
Julesntizimira <ntizimijules5@gmail.com>
2023-11-21 08:26:23 +0200

    fixi socket system

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,6 +15,8 @@ import random
 from string import ascii_uppercase
 
 
+
+
 from api.v1.views import app_views
 
 app = Flask(__name__)
@@ -23,22 +25,6 @@ app.config['SECRET_KEY'] = 'thisisasecretkey'
 app.config['SESSION_PROTECTION'] = 'strong'
 
 socketio = SocketIO(app)
-
-
-rooms = {}
-
-def generate_unique_code(length):
-    while True:
-        code = ""
-        for _ in range(length):
-            code += random.choice(ascii_uppercase)
-        if code not in rooms:
-            break
-    return code
-
-
-
-
 
 
 login_manager = LoginManager()
@@ -100,7 +86,7 @@ def not_found(error):
     return make_response(jsonify({'error': "Not found"}), 404)
 
 @app.route('/')
-def home():
+def landing():
     return render_template('landing1.html')
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -176,8 +162,23 @@ def itemInfos():
     return render_template('item.html')
 
 
-@app.route("/user", methods=["POST", "GET"])
-def user():
+
+
+rooms = {}
+
+def generate_unique_code(length):
+    while True:
+        code = ""
+        for _ in range(length):
+            code += random.choice(ascii_uppercase)
+        
+        if code not in rooms:
+            break
+    
+    return code
+
+@app.route("/home", methods=["POST", "GET"])
+def home():
     session.clear()
     if request.method == "POST":
         name = request.form.get("name")
@@ -186,30 +187,29 @@ def user():
         create = request.form.get("create", False)
 
         if not name:
-            return render_template("user.html", error="Please enter a name.", code=code, name=name)
+            return render_template("home.html", error="Please enter a name.", code=code, name=name)
 
         if join != False and not code:
-            return render_template("user.html", error="Please enter a room code.", code=code, name=name)
+            return render_template("home.html", error="Please enter a room code.", code=code, name=name)
         
         room = code
         if create != False:
             room = generate_unique_code(4)
             rooms[room] = {"members": 0, "messages": []}
         elif code not in rooms:
-            return render_template("user.html", error="Room does not exist.", code=code, name=name)
+            return render_template("home.html", error="Room does not exist.", code=code, name=name)
         
         session["room"] = room
         session["name"] = name
         return redirect(url_for("room"))
 
-    return render_template("user.html")
-
+    return render_template("home.html")
 
 @app.route("/room")
 def room():
     room = session.get("room")
     if room is None or session.get("name") is None or room not in rooms:
-        return redirect(url_for("user"))
+        return redirect(url_for("home"))
 
     return render_template("room.html", code=room, messages=rooms[room]["messages"])
 
@@ -255,6 +255,7 @@ def disconnect():
     
     send({"name": name, "message": "has left the room"}, to=room)
     print(f"{name} has left the room {room}")
+
 
 
 
