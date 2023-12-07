61d189b1de37f8df4201949752c5cf7a3cf7ec6a
Julesntizimira <ntizimijules5@gmail.com>
2023-12-07 14:54:56 +0200

    correct typo in create room api

--- webdynamic/app.py
+++ webdynamic/app.py
@@ -16,7 +16,7 @@ app.config['SECRET_KEY'] = 'thisisasecretkey'
 socketio = SocketIO(app)
 
 app.register_blueprint(app_pages)
-CORS(app, resources={r"/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"]}})
 
 login_manager = LoginManager()
 login_manager.init_app(app)
